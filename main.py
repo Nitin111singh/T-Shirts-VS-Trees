54e4ce2ccd1efd680c6b00d34445dd0e53872acf
Nitin111singh <nitinsingh80663@gmail.com>
2023-10-14 04:54:21 +0530

    written clean code instead of brute one

--- main.py
+++ main.py
@@ -3,47 +3,28 @@ from bs4 import BeautifulSoup
 
 URL = "https://hacktoberfest.com/participation/"
 page = requests.get(URL)
-
 soup = BeautifulSoup(page.content, "html.parser")
-treeCount = 0
-tShirtCount = 0
-for element in soup.find_all(string=lambda string: string and "tree" in string):
-    treeCount += 1
-    #print(element.get_text())
-for element in soup.find_all(string=lambda string: string and "trees" in string):
-    treeCount += 1    
-for element in soup.find_all(string=lambda string: string and "TREE" in string):
-    treeCount += 1
-for element in soup.find_all(string=lambda string: string and "Tree" in string):
-    treeCount += 1
-for element in soup.find_all(string=lambda string: string and "TREES" in string):
-    treeCount += 1
-for element in soup.find_all(string=lambda string: string and "Trees" in string):
-    treeCount += 1
 
-for element in soup.find_all(string=lambda string: string and "t-shirt" in string):
-    tShirtCount += 1
-for element in soup.find_all(string=lambda string: string and "t-shirts" in string):
-    tShirtCount += 1
-for element in soup.find_all(string=lambda string: string and "T-shirt" in string):
-    tShirtCount += 1
-for element in soup.find_all(string=lambda string: string and "T-shirts" in string):
-    tShirtCount += 1
-for element in soup.find_all(string=lambda string: string and "T-Shirt" in string):
-    tShirtCount += 1
-for element in soup.find_all(string=lambda string: string and "T-Shirts" in string):
-    tShirtCount += 1
-for element in soup.find_all(string=lambda string: string and "T-SHIRT" in string):
-    tShirtCount += 1
-for element in soup.find_all(string=lambda string: string and "T-SHIRTS" in string):
-    tShirtCount += 1
+tree_variations = ["tree", "trees", "TREE", "Tree", "TREES", "Trees"]
+tshirt_variations = ["t-shirt", "t-shirts", "T-shirt", "T-shirts", "T-Shirt", "T-Shirts", "T-SHIRT", "T-SHIRTS"]
 
-print("Counted Trees" , treeCount)
-print("Counted T Shirts" , tShirtCount)
+tree_count = 0
+tshirt_count = 0
 
-if (treeCount > tShirtCount):
+for word in tree_variations:
+    for element in soup.find_all(string=lambda string: string and word in string):
+        tree_count += 1
+
+for word in tshirt_variations:
+    for element in soup.find_all(string=lambda string: string and word in string):
+        tshirt_count += 1
+
+print("Counted Trees:", tree_count)
+print("Counted T-Shirts:", tshirt_count)
+
+if tree_count > tshirt_count:
     print("Trees Win!")
-if (treeCount == tShirtCount):
+elif tree_count == tshirt_count:
     print("It's a tie!")
 else:
-    print("T Shirts Win!")
+    print("T-Shirts Win!")
